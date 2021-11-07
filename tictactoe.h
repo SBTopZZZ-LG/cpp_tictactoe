f7fcb21fc7df15262e4a0944016bb774a6293204
Saumitra Topinkatti <saumitra@SBTOPZZZ-MBP.local>
2021-11-07 17:34:42 +0530

    Added destructor to Board_3x3

--- tictactoe.h
+++ tictactoe.h
@@ -202,6 +202,12 @@ public:
                 }
             }
     }
+    ~Board_3x3()
+    {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                delete board[i][j];
+    }
 
     bool setMark(int pos)
     {
